9082fb70d4fc316cac40361b109e74d2110d4534
Maria Clara <mcbarroso.contato@gmail.com>
2022-03-29 20:39:07 -0300

    Challenge complete L4

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
-        }
-    }
+  data() {
+		return {
+			product: 'Socks',
+			image: './assets/images/socks_blue.jpg',
+			image2: './assets/images/socks_green.jpg',
+			inventory: 100,
+			onSale: false
+		}
+	}
 })
