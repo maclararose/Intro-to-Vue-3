bac400642eb6ee5e846b87eb9b268a19dce165f9
Maria Clara <mcbarroso.contato@gmail.com>
2022-03-29 20:07:23 -0300

    Challenge complete L3

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            // solution
+            description: 'Nerd socks are so cool that warm.',
             url: 'https://www.vuemastery.com/'
-            // solution
         }
     }
 })
