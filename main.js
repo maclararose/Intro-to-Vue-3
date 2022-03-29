990bbc5903af0f3ecc5c693bfc8d2b5b23a7a02c
Maria Clara <mcbarroso.contato@gmail.com>
2022-03-29 17:37:32 -0300

    Firts commit and projetc Vue

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Nerd socks are so cool that make you warm.'
+    }
+  },
+})
